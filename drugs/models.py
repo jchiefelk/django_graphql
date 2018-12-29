53912b0a14b0414f72fec2c8b93bf512e41c786e
Jackson Chief Elk <chiefelk@Jacksons-MacBook-Pro.local>
2018-12-28 20:23:23 -0800

    Add table name change and linted code

--- drugs/models.py
+++ drugs/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class Drugs(models.Model):
-  name = models.CharField(max_length=50, null=True)
-  in_chi = models.CharField(max_length=200, null=True)
 
-  def __str__(self):
-    return self.name
+# Create your models here.
+class Drug(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    in_chi = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
